5f81325d12f76da078c5acc8dc328c20c1d98cf9
Jeff Kluge <jeffkl@microsoft.com>
2018-03-30 07:08:04 -0700

    Add first unit test to Traversal (#17)
    * Using MSBuildLocator and MSBuildProjectCreator to abstract away projects
    * Fix some issues in traversal

---
+++ src/Traversal.UnitTests/MSBuildTestBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Build.Locator;
+
+namespace Microsoft.Build.Traversal.UnitTests
+{
+    public abstract class MSBuildTestBase
+    {
+        public static readonly VisualStudioInstance CurrentVisualStudioInstance = MSBuildLocator.RegisterDefaults();
+
+        protected MSBuildTestBase()
+        {
+            MSBuildPath = CurrentVisualStudioInstance.MSBuildPath;
+        }
+
+        protected string MSBuildPath { get; }
+    }
+}
