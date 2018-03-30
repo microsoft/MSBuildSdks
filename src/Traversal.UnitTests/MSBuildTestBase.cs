7532f29ebb255bd0be15ec83ba06250a6b5b2e99
Jeff Kluge <jeffkl@microsoft.com>
2018-03-30 07:03:18 -0700

    wip

--- src/Traversal.UnitTests/MSBuildTestBase.cs
+++ src/Traversal.UnitTests/MSBuildTestBase.cs
@@ -6,5 +6,12 @@ namespace Microsoft.Build.Traversal.UnitTests
     public abstract class MSBuildTestBase
     {
         public static readonly VisualStudioInstance CurrentVisualStudioInstance = MSBuildLocator.RegisterDefaults();
+
+        protected MSBuildTestBase()
+        {
+            MSBuildPath = CurrentVisualStudioInstance.MSBuildPath;
+        }
+
+        protected string MSBuildPath { get; }
     }
 }
