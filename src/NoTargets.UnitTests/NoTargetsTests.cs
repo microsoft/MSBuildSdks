c7778ad003fa09414e435402516cf0fa4dd9be19
Jeff Kluge <jeffkl@microsoft.com>
2018-04-06 08:19:31 -0700

    Add unit tests for NoTargets

---
+++ src/NoTargets.UnitTests/NoTargetsTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Traversal.UnitTests;
+using Microsoft.Build.Utilities.ProjectCreation;
+using Shouldly;
+using Xunit;
+
+namespace Microsoft.Build.NoTargets.UnitTests
+{
+    public class NoTargetsTests : MSBuildSdkTestBase
+    {
+        [Fact]
+        public void SimpleBuild()
+        {
+            ProjectCreator.Templates.NoTargetsProject(
+                path: GetTempFileWithExtension(".csproj"),
+                customAction: creator =>
+                {
+                    creator.Target("TakeAction", afterTargets: "Build")
+                        .TaskMessage("86F00AF59170450E9D687652D74A6394", MessageImportance.High);
+                })
+                .Property("GenerateDependencyFile", "false")
+                .Save()
+                .TryBuild("Build", out bool result, out BuildOutput buildOutput);
+
+            result.ShouldBeTrue(() => buildOutput.GetConsoleLog());
+
+            buildOutput
+                .MessagesHighImportance
+                 .Select(i => i.Message)
+                .ToList()
+                .ShouldContain("86F00AF59170450E9D687652D74A6394");
+        }
+    }
+}
---
+++ src/NoTargets.UnitTests/ProjectCreatorTemplates.cs
@@ -0,0 +1,39 @@
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Utilities.ProjectCreation;
+using System;
+using System.IO;
+
+namespace Microsoft.Build.NoTargets.UnitTests
+{
+    public static class CustomProjectCreatorTemplates
+    {
+        public static ProjectCreator NoTargetsProject(
+            this ProjectCreatorTemplates templates,
+            Action<ProjectCreator> customAction = null,
+            string path = null,
+            string defaultTargets = null,
+            string initialTargets = null,
+            string sdk = null,
+            string toolsVersion = null,
+            string treatAsLocalProperty = null,
+            ProjectCollection projectCollection = null,
+            NewProjectFileOptions? projectFileOptions = NewProjectFileOptions.None)
+        {
+            string currentDirectory = Environment.CurrentDirectory;
+
+            return ProjectCreator.Create(
+                    path,
+                    defaultTargets,
+                    initialTargets,
+                    sdk,
+                    toolsVersion,
+                    treatAsLocalProperty,
+                    projectCollection,
+                    projectFileOptions)
+                .Import(Path.Combine(currentDirectory, "Sdk", "Sdk.props"))
+                .Property("TargetFramework", "netstandard1.0")
+                .CustomAction(customAction)
+                .Import(Path.Combine(currentDirectory, "Sdk", "Sdk.targets"));
+        }
+    }
+}
