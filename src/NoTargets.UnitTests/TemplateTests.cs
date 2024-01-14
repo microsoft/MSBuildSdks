026c328b6faa579bddc4a921bb8f02c9e22e439d
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-01-14 15:10:43 -0500

    Add NoTagets template

---
+++ src/NoTargets.UnitTests/TemplateTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Licensed under the MIT license.
+
+#if NET8_0_OR_GREATER
+using Microsoft.Extensions.Logging;
+using Microsoft.TemplateEngine.Authoring.TemplateVerifier;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Build.NoTargets.UnitTests
+{
+    public class TemplateTests
+    {
+        private static readonly string RootPath = Path.GetDirectoryName(typeof(TemplateTests).Assembly.Location);
+        private readonly ILoggerFactory _loggerFactory;
+
+        public TemplateTests(ITestOutputHelper xunitOutputHelper)
+        {
+            _loggerFactory = LoggerFactory.Create(config =>
+            {
+                config.AddXunit(xunitOutputHelper);
+            });
+        }
+
+        [Fact]
+        public async Task UsesDirectoryAsDefaultName()
+        {
+            TemplateVerifierOptions options = new (templateName: "notargets")
+            {
+                TemplatePath = Path.Combine(RootPath, "Templates", "notargets"),
+            };
+
+            VerificationEngine engine = new (_loggerFactory);
+            await engine.Execute(options);
+        }
+
+        [Fact]
+        public async Task RespectsExplicitName()
+        {
+            TemplateVerifierOptions options = new (templateName: "notargets")
+            {
+                TemplateSpecificArgs = new[] { "--name", "asdf" },
+                TemplatePath = Path.Combine(RootPath, "Templates", "notargets"),
+            };
+
+            VerificationEngine engine = new (_loggerFactory);
+            await engine.Execute(options);
+        }
+    }
+}
+#endif
---
+++ src/NoTargets/Templates/README.md
@@ -0,0 +1,24 @@
+# Microsoft.Build.Traversal.Templates
+
+## Installing the templates
+
+The templates are available on [NuGet](https://www.nuget.org/packages/Microsoft.Build.NoTargets.Templates/).
+To install the templates, run the following command:
+
+```bash
+dotnet new -i Microsoft.Build.NoTargets.Templates
+```
+
+## Using the templates
+
+Creating a new project with the default name
+
+```bash
+dotnet new notargets
+```
+
+Creating a new project "AdventureWorks" (`-n` or `--name`):
+
+```bash
+dotnet new notargets -n "AdventureWorks"
+```
