48931d2669fa40de04889422def7c5e7500a6fa6
Erik Mavrinac <erikmav@microsoft.com>
2024-01-18 09:32:06 -0800

    Fix missing LastWriteTime update in CoW SDK causing excess recopying in incremental builds (#530)

--- src/CopyOnWrite/Copy.cs
+++ src/CopyOnWrite/Copy.cs
@@ -333,7 +333,7 @@ namespace Microsoft.Build.Tasks
                 bool cloned = false;
                 if (IsCopyOnWriteSupported(sourceFilePath, destinationFilePath))
                 {
-                    cloned = TryCopyOnWrite(sourceFilePath, destinationFilePath);
+                    cloned = TryCopyOnWrite(sourceFileState, destinationFilePath);
                 }
 
                 if (!cloned)
@@ -986,14 +986,16 @@ namespace Microsoft.Build.Tasks
         /// <summary>
         /// Attempt to clone source file to destination.
         /// </summary>
-        /// <param name="sourceFullPath">Full path to source file.</param>
-        /// <param name="destFullPath">full path to destination file.</param>
+        /// <param name="sourceFileState">Source file information.</param>
+        /// <param name="destFullPath">Full path to destination file.</param>
         /// <returns>True when a CloneFile was successfully performed.</returns>
-        private bool TryCopyOnWrite(string sourceFullPath, string destFullPath)
+        private bool TryCopyOnWrite(FileState sourceFileState, string destFullPath)
         {
+            string sourceFullPath = sourceFileState.FullPath;
             try
             {
                 CoW.CloneFile(sourceFullPath, destFullPath, CloneFlags.PathIsFullyResolved);
+                File.SetLastWriteTimeUtc(destFullPath, sourceFileState.LastWriteTimeUtcFast);  // Ensure up-to-date checks work.
                 Log.LogMessage(MessageImportance.Low, $"Created copy-on-write link '{sourceFullPath}' to '{destFullPath}'.");
             }
             catch (Exception e)
