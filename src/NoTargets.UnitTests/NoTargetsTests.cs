4c2739b30ea11fbf567172fed6c277eeb83d2949
stan-sz <37585349+stan-sz@users.noreply.github.com>
2024-09-24 12:44:20 +0200

    Fix NoTargetsTests.cs
    Signed-off-by: stan-sz <37585349+stan-sz@users.noreply.github.com>

--- src/NoTargets.UnitTests/NoTargetsTests.cs
+++ src/NoTargets.UnitTests/NoTargetsTests.cs
@@ -323,7 +323,7 @@ namespace Microsoft.Build.NoTargets.UnitTests
                             new[] { new ProjectGraphEntryPoint(project.FullPath) },
                             new[] { "Build" }));
 
-                    graphResult.OverallResult.ShouldBe(BuildResultCode.Success, buildOutput.GetConsoleLog());
+                    graphResult.OverallResult.ShouldBe(BuildResultCode.Success, graphResult.Exception.ToString());
                 }
                 finally
                 {
